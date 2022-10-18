8c1d19673fbf8bbc797383e5ef75c592b6753d95
seolhyelin <hyelin@bfactory.inc>
2022-10-18 17:57:46 +0900

    Feat: canvas 기쵸 공부즁

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
+ctx.moveTo(50, 50);
 
-//과정과 순서가 필수적!
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 
-ctx.fillStyle = "red";
 ctx.fill();
