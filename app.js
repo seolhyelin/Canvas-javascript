4982a8f828a987f08211d40a73605ec427ccbb4a
seolhyelin <tosiltosilkk@gmail.com>
2022-10-26 23:23:30 +0900

    Feat: 다채로운 선들로 페인팅하기

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
+const colors = [
+  "#f3a683",
+  "#f7d794",
+  "#778beb",
+  "#e77f67",
+  "#cf6a87",
+  "#786fa6",
+  "#f8a5c2",
+  "#63cdda",
+  "#f19066",
+  "#596275",
+];
 
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+ctx.lineWidth = 2;
 
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
