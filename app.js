20bd543c3515b353f849ece6ac0bba2eb2e4fc1b
seolhyelin <tosiltosilkk@gmail.com>
2022-10-31 23:11:38 +0900

    Feat: paint color

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
 
 canvas.width = 800;
 canvas.height = 800;
@@ -30,10 +32,21 @@ function cancelPainting() {
 function onLineWidthChange(event) {
   ctx.lineWidth = event.currentTarget.value;
 }
+function onColorChange(event) {
+  ctx.strokeStyle = ctx.fillStyle = event.target.value;
+}
 
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+
+  ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
