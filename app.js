eb0086b41dc3a5d5dea41b1171b9ec143cc04ef7
seolhyelin <tosiltosilkk@gmail.com>
2022-10-31 21:17:04 +0900

    Feat: line Width

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
 
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 
-ctx.lineWidth = 2;
 let isPainting = false;
 
 function onMove(event) {
@@ -24,9 +25,15 @@ function onMouseUp() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.currentTarget.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
